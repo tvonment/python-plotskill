a7fa1ffdb5e3741f5db155394df78676a40ae503
Thomas <65050898+tvonment@users.noreply.github.com>
2022-01-15 23:05:09 +0000

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 39783
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
+    # Callers to only those specified, '*' allows any caller.
+    # Example: os.environ.get("AllowedCallers", ["aaaaaaaa-1111-aaaa-aaaa-aaaaaaaa"])
+    ALLOWED_CALLERS = os.environ.get("AllowedCallers", ["*"])
---
+++ dialogs/main_dialog.py
@@ -0,0 +1,77 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+from botbuilder.dialogs import (
+    ComponentDialog,
+    WaterfallDialog,
+    WaterfallStepContext,
+    DialogTurnResult,
+    DialogTurnStatus,
+)
+from botbuilder.core import MessageFactory
+from botbuilder.schema import ActivityTypes, InputHints
+
+
+class MainDialog(ComponentDialog):
+    """
+    A root dialog that can route activities sent to the skill to different sub-dialogs.
+    """
+
+    def __init__(self):
+        super().__init__(MainDialog.__name__)
+        self.add_dialog(
+            WaterfallDialog(WaterfallDialog.__name__, [self.process_activity])
+        )
+
+        self.initial_dialog_id = WaterfallDialog.__name__
+
+    async def process_activity(
+        self, step_context: WaterfallStepContext
+    ) -> DialogTurnResult:
+        current_activity_type = step_context.context.activity.type
+
+        # A skill can send trace activities, if needed.
+        await step_context.context.send_trace_activity(
+            f"{MainDialog.__name__}.process_activity()",
+            label=f"Got ActivityType: {current_activity_type}",
+        )
+
+        if current_activity_type == ActivityTypes.event:
+            return await self._on_event_activity(step_context)
+        if current_activity_type == ActivityTypes.message:
+            return await self._on_message_activity(step_context)
+        else:
+            # We didn't get an activity type we can handle.
+            await step_context.context.send_activity(
+                MessageFactory.text(
+                    f'Unrecognized ActivityType: "{current_activity_type}".',
+                    input_hint=InputHints.ignoring_input,
+                )
+            )
+            return DialogTurnResult(DialogTurnStatus.Complete)
+
+    async def _on_event_activity(
+        self, step_context: WaterfallStepContext
+    ) -> DialogTurnResult:
+        """
+        This method performs different tasks based on the event name.
+        """
+        # We didn't get an activity name we can handle.
+        await step_context.context.send_activity(
+            MessageFactory.text(
+                'Python Dialog',
+                input_hint=InputHints.ignoring_input,
+            )
+        )
+        return DialogTurnResult(DialogTurnStatus.Complete)
+
+    async def _on_message_activity(
+        self, step_context: WaterfallStepContext
+    ) -> DialogTurnResult:
+        """
+        This method just gets a message activity and runs it through LUIS.
+        """        
+        await step_context.context.send_activity(
+            MessageFactory.text("some message", input_hint=InputHints.ignoring_input,)
+        )
+
+        return DialogTurnResult(DialogTurnStatus.Complete)
